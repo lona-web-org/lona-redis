365cc6f8d0f5da858a58a0d7f1157d1536c16293
SteveT <stock90975@gmail.com>
2023-04-20 10:11:07 +1000

    cleaned up debug, comments

--- lona_redis/middlewares.py
+++ lona_redis/middlewares.py
@@ -5,8 +5,9 @@ from loguru import logger
 
 
 class RedisSession:
+    # FIXME this isn't used - can be removed?
     # def __init__(self, redis_user):
-    # self.redis_user = redis_user
+    #   self.redis_user = redis_user
 
     def __init__(self, *args, **kwargs):
         """
@@ -19,29 +20,27 @@ class RedisSession:
         https://redis.readthedocs.io/en/latest/connections.html
         """
 
-        # logger.debug("init RedisSession")
         # FIXME pass kwargs straight through to redis.Redis?
         self.r = redis.Redis(**kwargs)
-        self.user_request_session_key = None
 
     def _actual_redis_key(self, user_key):
         """
         combine self.user_request_session_key with user's key
         so that the ACTUAL key used to store value in Redis is unique
         """
-        user_request_session_key = self.user_request_session_key
-        return user_request_session_key + ":" + user_key
+        COMBINE_CHR = ":"
+        return self.user_request_session_key + COMBINE_CHR + user_key
 
     def set(self, *args, **kwargs):
         """
-        let user easily set values
+        user should call this to easily set values
         eg. request.user.session.set(foo=123)
+
+        pickle all values so that Redis can store any pickle-able Python value
         """
-        # logger.debug(f"set RedisSession {args=} {kwargs=}")
         for user_key, value in kwargs.items():
             actual_redis_key = self._actual_redis_key(user_key)
             # logger.debug(f"{user_key=} {actual_redis_key=} {value=}")
-            # self.r.set(actual_redis_key, value)
             self.r.set(actual_redis_key, pickle.dumps(value))
 
     # def get(self, *args, **kwargs):
@@ -49,17 +48,19 @@ class RedisSession:
 
     def get(self, *args):
         """
-        let user easily get values
+        user should call this to easily get values
         eg. request.user.session.get("foo")
+
+        un-pickle all values that were retrieved from Redis
         """
         user_key = args[0]
         actual_redis_key = self._actual_redis_key(user_key)
         # logger.debug(f"{user_key=} {actual_redis_key=}")
 
-        # return self.r.get(actual_redis_key)
         return pickle.loads(self.r.get(actual_redis_key))
 
 
+# FIXME this isn't used - can be removed?
 class RedisUser:
     def __init__(self, connection):
         # self.connection = connection
@@ -78,7 +79,7 @@ class RedisSessionMiddleware:
 
         settings = data.server.settings
 
-        # FIXME FIXME FIXME
+        # FIXME
         # TODO what should the settings be?
         # TODO there are many kwargs (https://redis.readthedocs.io/en/latest/connections.html)
         # TODO maybe settings should be a dict to override the defaults?
@@ -89,10 +90,11 @@ class RedisSessionMiddleware:
         # default args
         # self.redis_session = RedisSession(host="localhost", port=6379, db=0)
 
-        # FIXME pass kwargs (see note above)
+        # FIXME pass kwargs (see above)
         self.redis_session = RedisSession()
 
-        # to be set in handle_request() just prior to allowing user to call get(), set()
+        # to be set in handle_request()
+        # just prior to user calling request.user.session.get(), request.user.session.set()
         self.user_request_session_key = None
 
         return data
