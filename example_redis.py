bb704f0906178566dc4213f0622296db57ece92b
SteveT <stock90975@gmail.com>
2023-04-20 10:10:09 +1000

    better examples

--- example_redis.py
+++ example_redis.py
@@ -24,6 +24,10 @@ app.settings.REDIS_PASSWORD = "abcd1234"
 @app.route("/")
 class Index(View):
     def handle_request(self, request):
+        #
+        # NOTE examples of using request.user.session.set, request.user.session.get
+        #
+
         # request.user.session.set(foo=123)
         # OR
         # foo = 123
@@ -32,13 +36,44 @@ class Index(View):
         # logger.debug(f"{foo=}")
         # logger.debug(f"{request.user.session._actual_redis_key('foo')=}")
 
-        list_var = [1, 2.222, "string"]
-        int_var = 123
-        float_var = 123.456
-        tuple_var = (1, 2, 3)
-        dict_var = {"a": 1, "b": 2}
+        request.user.session.set(str_var="hello world")
+        request.user.session.set(list_var=[1, 2.222, "hello world"])
+        request.user.session.set(int_var=123)
+        request.user.session.set(float_var=123.456)
+        request.user.session.set(tuple_var=(1, 2, 3))
+        request.user.session.set(dict_var={"a": 1, "b": 2})
+        request.user.session.set(boolean_var=True)
+        request.user.session.set(
+            mixed_var_1=[
+                True,
+                {"a": 1, "b": 2},
+                (1, 2, 3),
+                123.456,
+                123,
+                [1, 2.222, "hello world"],
+                "hello world",
+            ]
+        )
+        request.user.session.set(
+            mixed_var_2={
+                "a": [1, 2, 3],
+                "b": {"a": 1, "b": 2},
+            }
+        )
 
-        # NOTE example of using Redis commands directly
+        logger.debug(f"{request.user.session.get('str_var')=}")
+        logger.debug(f"{request.user.session.get('list_var')=}")
+        logger.debug(f"{request.user.session.get('int_var')=}")
+        logger.debug(f"{request.user.session.get('float_var')=}")
+        logger.debug(f"{request.user.session.get('tuple_var')=}")
+        logger.debug(f"{request.user.session.get('dict_var')=}")
+        logger.debug(f"{request.user.session.get('boolean_var')=}")
+        logger.debug(f"{request.user.session.get('mixed_var_1')=}")
+        logger.debug(f"{request.user.session.get('mixed_var_2')=}")
+
+        #
+        # NOTE examples of using Redis commands directly
+        #
         # https://redis.readthedocs.io/en/latest/commands.html#core-commands
         request.user.session.r.set("myKey", "thevalueofmykey")
         myKey = request.user.session.r.get("myKey")
@@ -48,7 +83,7 @@ class Index(View):
         # return HTML(H1(request.user.session["count"]))
         return HTML(
             H1("Hello World"),
-            #    P("Lorem Ipsum"),
+            # P("Lorem Ipsum"),
         )
 
 
